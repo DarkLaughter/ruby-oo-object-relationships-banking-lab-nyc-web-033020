e400c531aa5a2ce932bd23fa71c1d946a0527fea
github username <github email address>
2020-04-02 14:31:39 -0400

    partially completed transfer.rb

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,21 @@
 class Transfer
-  # your code here
+
+  attr_reader :sender, :receiver, :status
+  attr_accessor :transfer_amount
+
+  def initialize (sender, receiver, transfer_amount)
+    @sender = sender 
+    @receiver = receiver
+    @status = "pending"
+    @transfer_amount = transfer_amount
+  end
+
+  def amount
+    @transfer_amount
+  end
+
+  def valid?
+    
+  end
+
 end
